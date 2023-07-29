4b3527f95e5130075b0c57a8bdbb187eb1ec4986
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-07-29 10:54:30 -0300

    Finalizei a parte de post

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./Paginas/Inicio";
 import SobreMim from "./Paginas/SobreMim";
-import Menu from "./componentes/Menu";
+import Menu from "./componentes/Menu";  
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "Paginas/Post";
 
 function AppRoutes() {
   return (
@@ -14,6 +15,7 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path= "Sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post/>}/>
         </Route>
 
         <Route path="*" element={<div>Página não encontrada</div>} />
